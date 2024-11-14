828a7ab5139bd8373bf34942b72faccb4934fd4f
Andrew Hastings <abh@ieee.org>
2024-11-14 08:19:14 -0600

    Factor out common code for expanding compressed text.

--- opl.c
+++ opl.c
@@ -33,6 +33,122 @@
 #include "simtap.h"
 
 
+/* set lastmask = 040 for UPDATE PLs */
+/* returns -2 if early EOR, -1 if not found, else mod number */
+int read_hist(cdc_ctx_t *cd, char *cp, int idx, int lastmask)
+{
+	int rv = -1;
+	int hist;
+
+	/* iterate through modification history "bytes" (18 bits) */
+	while (1) {
+		hist = (cp[idx] << 12) | (cp[idx + 1] << 6) | cp[idx + 2];
+
+		/* end of history "bytes"? */
+		if (!hist)
+			break;
+
+		/* bit 16 = activated the line */
+		if (hist & 0200000)
+			rv = hist & 0177777;
+
+		idx += 3;
+		if (idx > 9) {
+			/* last history word? */
+			if (cp[0] & lastmask)
+				break;
+
+			if (!(cp = cdc_getword(cd)))
+				return -2;
+			idx = 1;
+		}
+	}
+
+	return rv;
+}
+
+
+#define MAXLEN		    160	    /* max expanded line length */
+
+#define EXPAND_IS_64	    1
+#define EXPAND_63_IS_COL    2	    /* only for MODIFY OPL */
+
+/* return -1=line too long, -2=EOR, else remaining word count */
+int expand_text(cdc_ctx_t *cd, int wc, char *obuf, int flags)
+{
+	int state = 0;		/* 0=default, 1=00, 2=0077, 3=007700 */
+	int i;
+	char c, *cp, *op;
+
+	op = obuf;
+	for (; wc > 0; wc--) {
+		if (op - obuf > MAXLEN)	/* line length exceeded? */
+			return -1;
+		if (!(cp = cdc_getword(cd)))
+			return -2;
+		dprint(("expand_text: cp=%p wc=%d\n", cp, wc));
+		for (i = 0; i < 10; i++) {
+			c = cp[i];
+			dprint(("expand_text: state=%d c=%d\n", state, c));
+			if (c == 0) {
+				/* 0000 = end-of-line */
+				if (state == 1)
+					break;
+				/* 0077 -> 007700 transition? */
+				if (state == 2) {
+					state = 3;
+					continue;
+				}
+				/* 00770000 is invalid; treat as 00 */
+				if (state == 3)
+					dprint(("expand_text: 00770000\n"));
+				/* single 00 */
+				state = 1;
+				continue;
+			}
+
+			/* 0001 expansion depends on OPL charset */
+			if (state == 1 && c == 1 && (flags & EXPAND_IS_64)) {
+				/* 64-character set ':' */
+				*op++ = dcmap[0];
+				state = 0;
+				continue;
+			}
+
+			/* 00xx or 007700xx: expand spaces */
+			if (state == 1 || state == 3) {
+				int j;
+
+				state = 0;
+				if (op - obuf + c > MAXLEN)
+					return -1;
+				for (j = 0; j < c+1; j++)
+					*op++ = ' ';
+				if (c == 077)
+					state = 2;
+				continue;
+			}
+
+			/* xx or 0077xx: normal char */
+			state = 0;
+			/* 063 is always ':' if OPL charset is 63 */
+			*op++ = c == 063 && (flags & EXPAND_63_IS_COL)
+					? ':'
+					: dcmap[c];
+		}
+
+		/* EOL marker found: mark word as consumed, exit loop */
+		if (i < 10) {
+			wc--;
+			break;
+		}
+	}
+	*op = '\0';
+
+	return wc;
+}
+
+
 /* MODIFY OPL/OPLC */
 char *extract_opl(cdc_ctx_t *cd, char *name)
 {
@@ -41,7 +157,7 @@ char *extract_opl(cdc_ctx_t *cd, char *name)
 	char fname[16], deck[8];
 	char *cp, *mods;
 	int i, len, nmods, nread;
-	int is_64 = 0, is_ascii = 0;
+	int is_ascii = 0, flags = EXPAND_63_IS_COL;
 	int width = 72, seqon = 1;
 
 	dprint(("extract_opl: %s\n", name));
@@ -83,14 +199,14 @@ char *extract_opl(cdc_ctx_t *cd, char *name)
 		if (!(cp = cdc_getword(cd)))
 			return "EOR reading charset from 7700 table";
 		if (cp[8] <= 1 && cp[9] == 064)
-			is_64 = 1;
+			flags = EXPAND_IS_64;
 		if (cp[8] == 1 && (cp[9] == 0 || cp[9] == 064))
 			is_ascii = 1;
 		nread = 14;
 	}
 
-	dprint(("extract_opl: nread %d ascii %d 64 %d\n",
-		 nread, is_ascii, is_64));
+	dprint(("extract_opl: nread %d ascii %d flags %d\n",
+		 nread, is_ascii, flags));
 	if (!cdc_skipwords(cd, len - nread))
 		return "EOR skipping over 7700 table";
 
@@ -120,44 +236,22 @@ char *extract_opl(cdc_ctx_t *cd, char *name)
 
 	/* iterate through text lines */
 	while (cp = cdc_getword(cd)) {
-		int active, wc, seq, hist;
-		char *modname;
-		int state;
-		char c;
-		char obuf[172], *op;
+		int active, wc, seq, modnum;
+		char *modname = "unknown";
+		char obuf[MAXLEN + 12];
 
 		active = cp[0] & 040;
 		wc = cp[0] & 037;
 		seq = (cp[1] << 12) | (cp[2] << 6) | cp[3];
-		modname = "unknown";
 
-		/* process modification history "bytes" (18 bits) */
-		i = 4;
-		while (1) {
-			hist = (cp[i] << 12) | (cp[i + 1] << 6) | cp[i + 2];
-
-			/* end of history "bytes"? */
-			if (!hist)
-				break;
-
-			/* bit 16 = activated the line */
-			if (hist & 0200000) {
-				hist &= 0177777;
-				modname = hist < nmods ? mods + hist * 8
-						       : "invalid";
-			}
-
-			i += 3;
-			if (i > 9) {
-				if (!(cp = cdc_getword(cd)))
-					break;
-				i = 1;
-			}
-		}
-		if (!cp) {
+		/* first history "byte" (18 bits) is bits 35-18 */
+		modnum = read_hist(cd, cp, 4, 0);
+		if (modnum == -2) {
 			out_close(of);
 			return "EOR reading modification history";
 		}
+		if (modnum >= 0)
+			modname = modnum < nmods ? mods + modnum*8 : "invalid";
 
 		/* skip over inactive line */
 		if (!active) {
@@ -169,77 +263,14 @@ char *extract_opl(cdc_ctx_t *cd, char *name)
 		dprint(("extract_opl: line %s:%d wc=%d\n", modname, seq, wc));
 
 		/* process compressed text */
-		state = 0;	/* 0=default, 1=00, 2=0077, 3=007700 */
-		op = obuf;
-		for (; wc > 0; wc--) {
-			if (op - obuf > 160)	/* line length exceeded? */
-				break;
-			if (!(cp = cdc_getword(cd)))
-				break;
-			dprint(("extract_opl: cp=%p wc=%d\n", cp, wc));
-			for (i = 0; i < 10; i++) {
-				c = cp[i];
-				dprint(("extract_opl: state=%d c=%d\n",
-					 state, c));
-				if (c == 0) {
-					/* 0000 = end-of-line */
-					if (state == 1)
-						break;
-					/* 0077 -> 007700 transition? */
-					if (state == 2) {
-						state = 3;
-						continue;
-					}
-					/* 00770000 is invalid; treat as 00 */
-					if (state == 3)
-						dprint(("extract_opl: "
-							"00770000\n"));
-					/* single 00 */
-					state = 1;
-					continue;
-				}
+		wc = expand_text(cd, wc, obuf, flags);
 
-				/* 0001 expansion depends on OPL charset */
-				if (is_64 && state == 1 && c == 1) {
-					/* 64-character set ':' */
-					*op++ = dcmap[0];
-					state = 0;
-					continue;
-				}
-
-				/* 00xx or 007700xx: expand spaces */
-				if (state == 1 || state == 3) {
-					int j;
-
-					state = 0;
-					if (op - obuf + c > 160)
-						break;
-					for (j = 0; j < c+1; j++)
-						*op++ = ' ';
-					if (c == 077)
-						state = 2;
-					continue;
-				}
-
-				/* xx or 0077xx: normal char */
-				state = 0;
-				/* 063 is always ':' unless OPL charset is 64 */
-				*op++ = c == 063 && !is_64 ? ':' : dcmap[c];
-			}
-
-			/* EOL marker found: mark word as consumed, exit loop */
-			if (i < 10) {
-				wc--;
-				break;
-			}
-		}
-
-		if (!cp) {
+		if (wc == -2) {
 			out_close(of);
 			return "EOR reading compressed text";
 		}
 
-		if (op - obuf > 160) {
+		if (wc == -1) {
 			out_close(of);
 			return "line too long in compressed text";
 		}
@@ -250,7 +281,6 @@ char *extract_opl(cdc_ctx_t *cd, char *name)
 		}
 
 		/* TBD: *SEQ, *NOSEQ, *WIDTH n */
-		*op = '\0';
 		if (verbose && seqon || verbose > 1)
 			fprintf(of, "%-*.*s%-7s%6d\n", width, width, obuf,
 						       modname, seq);
@@ -273,7 +303,7 @@ char *extract_upl(cdc_ctx_t *cd, char *name)
 	char *cp;
 	char *ids;
 	int width = verbose > 1 ? 80 : 72;
-	int is_64;
+	int flags = 0;
 	int i, deckcnt, idcnt;
 
 	dprint(("extract_upl: %s\n", name));
@@ -282,7 +312,8 @@ char *extract_upl(cdc_ctx_t *cd, char *name)
 	cp = cdc_getword(cd);
 	if (!cp || memcmp(cp, "\003\010\005\003\013", 5) != 0 || (cp[5] & 076))
 		return "invalid OLDPL header";
-	is_64 = (cp[6] != 036);	    /* '3' */
+	if (cp[6] != 036)	    /* '3' */
+		flags = EXPAND_IS_64;
 
 	if (!(cp = cdc_getword(cd)))
 		return "short OLDPL header";
@@ -314,49 +345,23 @@ char *extract_upl(cdc_ctx_t *cd, char *name)
 
 	/* iterate through text lines */
 	while (cp = cdc_getword(cd)) {
-		int active, wc, seq, hist;
-		int state;
-		char *modname;
-		char c;
-		char obuf[172], *op;
+		int active, wc, seq, modnum;
+		char *modname = "unknown";
+		char obuf[MAXLEN + 12];
 
 		active = cp[0] & 020;
 		wc = (cp[1] << 12) | (cp[2] << 6) | cp[3];
 		seq = (cp[4] << 12) | (cp[5] << 6) | cp[6];
-		modname = "unknown";
 
-		/* process modification history "bytes" (18 bits) */
-		i = 7;
-		while (1) {
-			hist = (cp[i] << 12) | (cp[i + 1] << 6) | cp[i + 2];
-
-			/* end of history "bytes"? */
-			if (!hist)
-				break;
-
-			/* bit 16 = activated the line */
-			if (hist & 0200000) {
-				hist &= 0177777;
-				modname = "invalid";
-				if (hist > 0 && hist <= idcnt)
-					modname = ids + 10 * (hist-1);
-			}
-
-			i += 3;
-			if (i > 9) {
-				/* last history word? */
-				if (cp[0] & 040)
-					break;
-
-				if (!(cp = cdc_getword(cd)))
-					break;
-				i = 1;
-			}
-		}
-		if (!cp) {
+		/* first history "byte" (18 bits) is bits 17-0 */
+		modnum = read_hist(cd, cp, 7, 040);
+		if (modnum == -2) {
 			out_close(of);
 			return "EOR reading modification history";
 		}
+		if (modnum > 0)
+			modname = modnum <= idcnt ? ids + 10 * (modnum - 1)
+						  : "invalid";
 
 		/* skip over inactive line */
 		if (!active) {
@@ -368,76 +373,14 @@ char *extract_upl(cdc_ctx_t *cd, char *name)
 		dprint(("extract_uplr: line %s:%d wc=%d\n", modname, seq, wc));
 
 		/* process compressed text */
-		state = 0;	/* 0=default, 1=00, 2=0077, 3=007700 */
-		op = obuf;
-		for (; wc > 0; wc--) {
-			if (op - obuf > 160)	/* line length exceeded? */
-				break;
-			if (!(cp = cdc_getword(cd)))
-				break;
-			dprint(("extract_uplr: cp=%p wc=%d\n", cp, wc));
-			for (i = 0; i < 10; i++) {
-				c = cp[i];
-				dprint(("extract_uplr: state=%d c=%d\n",
-					 state, c));
-				if (c == 0) {
-					/* 0000 = end-of-line */
-					if (state == 1)
-						break;
-					/* 0077 -> 007700 transition? */
-					if (state == 2) {
-						state = 3;
-						continue;
-					}
-					/* 00770000 is invalid; treat as 00 */
-					if (state == 3)
-						dprint(("extract_uplr: "
-							"00770000\n"));
-					/* single 00 */
-					state = 1;
-					continue;
-				}
+		wc = expand_text(cd, wc, obuf, flags);
 
-				/* 0001 expansion depends on OLDPL charset */
-				if (is_64 && state == 1 && c == 1) {
-					/* 64-character set ':' */
-					*op++ = dcmap[0];
-					state = 0;
-					continue;
-				}
-
-				/* 00xx or 007700xx: expand spaces */
-				if (state == 1 || state == 3) {
-					int j;
-
-					state = 0;
-					if (op - obuf + c > 160)
-						break;
-					for (j = 0; j < c+1; j++)
-						*op++ = ' ';
-					if (c == 077)
-						state = 2;
-					continue;
-				}
-
-				/* xx or 0077xx: normal char */
-				state = 0;
-				*op++ = dcmap[c];
-			}
-
-			/* EOL marker found: mark word as consumed, exit loop */
-			if (i < 10) {
-				wc--;
-				break;
-			}
-		}
-
-		if (!cp) {
+		if (wc == -2) {
 			out_close(of);
 			return "EOR reading compressed text";
 		}
 
-		if (op - obuf > 160) {
+		if (wc == -1) {
 			out_close(of);
 			return "line too long in compressed text";
 		}
@@ -447,7 +390,6 @@ char *extract_upl(cdc_ctx_t *cd, char *name)
 			return "missing EOL in compressed text";
 		}
 
-		*op = '\0';
 		if (verbose)
 			fprintf(of, "%-*.*s%s.%d\n", width, width, obuf,
 						     modname, seq);
@@ -466,6 +408,7 @@ char *extract_uplr(cdc_ctx_t *cd, char *name)
 	FILE *of;
 	char fname[16];
 	char *cp;
+	int flags = (dcmap[063] == ':') ? 0 : EXPAND_IS_64;
 	int width = verbose > 1 ? 80 : 72;
 
 	/*
@@ -487,40 +430,16 @@ char *extract_uplr(cdc_ctx_t *cd, char *name)
 
 	/* iterate through text lines */
 	while (cp = cdc_getword(cd)) {
-		int active, wc, seq, hist;
-		int i, modnum, state;
-		char c;
-		char obuf[172], *op;
+		int active, wc, seq, modnum;
+		char obuf[MAXLEN + 12];
 
 		active = cp[0] & 020;
 		wc = (cp[1] << 12) | (cp[2] << 6) | cp[3];
 		seq = (cp[4] << 12) | (cp[5] << 6) | cp[6];
 
-		/* process modification history "bytes" (18 bits) */
-		i = 7;
-		while (1) {
-			hist = (cp[i] << 12) | (cp[i + 1] << 6) | cp[i + 2];
-
-			/* end of history "bytes"? */
-			if (!hist)
-				break;
-
-			/* bit 16 = activated the line */
-			if (hist & 0200000)
-				modnum = hist & 0177777;
-
-			i += 3;
-			if (i > 9) {
-				/* last history word? */
-				if (cp[0] & 040)
-					break;
-
-				if (!(cp = cdc_getword(cd)))
-					break;
-				i = 1;
-			}
-		}
-		if (!cp) {
+		/* first history "byte" (18 bits) is bits 17-0 */
+		modnum = read_hist(cd, cp, 7, 040);
+		if (modnum == -2) {
 			out_close(of);
 			return "EOR reading modification history";
 		}
@@ -535,76 +454,14 @@ char *extract_uplr(cdc_ctx_t *cd, char *name)
 		dprint(("extract_uplr: line d%06o:%d wc=%d\n", modnum, seq, wc));
 
 		/* process compressed text */
-		state = 0;	/* 0=default, 1=00, 2=0077, 3=007700 */
-		op = obuf;
-		for (; wc > 0; wc--) {
-			if (op - obuf > 160)	/* line length exceeded? */
-				break;
-			if (!(cp = cdc_getword(cd)))
-				break;
-			dprint(("extract_uplr: cp=%p wc=%d\n", cp, wc));
-			for (i = 0; i < 10; i++) {
-				c = cp[i];
-				dprint(("extract_uplr: state=%d c=%d\n",
-					 state, c));
-				if (c == 0) {
-					/* 0000 = end-of-line */
-					if (state == 1)
-						break;
-					/* 0077 -> 007700 transition? */
-					if (state == 2) {
-						state = 3;
-						continue;
-					}
-					/* 00770000 is invalid; treat as 00 */
-					if (state == 3)
-						dprint(("extract_uplr: "
-							"00770000\n"));
-					/* single 00 */
-					state = 1;
-					continue;
-				}
+		wc = expand_text(cd, wc, obuf, flags);
 
-				/* 0001 expansion depends on 64-charset */
-				if (state == 1 && c == 1 && dcmap[063] != ':') {
-					/* 64-character set ':' */
-					*op++ = dcmap[0];
-					state = 0;
-					continue;
-				}
-
-				/* 00xx or 007700xx: expand spaces */
-				if (state == 1 || state == 3) {
-					int j;
-
-					state = 0;
-					if (op - obuf + c > 160)
-						break;
-					for (j = 0; j < c+1; j++)
-						*op++ = ' ';
-					if (c == 077)
-						state = 2;
-					continue;
-				}
-
-				/* xx or 0077xx: normal char */
-				state = 0;
-				*op++ = dcmap[c];
-			}
-
-			/* EOL marker found: mark word as consumed, exit loop */
-			if (i < 10) {
-				wc--;
-				break;
-			}
-		}
-
-		if (!cp) {
+		if (wc == -2) {
 			out_close(of);
 			return "EOR reading compressed text";
 		}
 
-		if (op - obuf > 160) {
+		if (wc == -1) {
 			out_close(of);
 			return "line too long in compressed text";
 		}
@@ -614,7 +471,6 @@ char *extract_uplr(cdc_ctx_t *cd, char *name)
 			return "missing EOL in compressed text";
 		}
 
-		*op = '\0';
 		if (verbose)
 			fprintf(of, "%-*.*sd%06o.%d\n", width, width, obuf,
 							modnum, seq);
